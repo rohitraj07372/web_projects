1ef9099a650377f9b9f09e05cd15df8b31131138
Rohit raj <rr2542333@gmail.com>
2021-05-07 08:57:18 +0530

    clock

---
+++ digitalClock/script.js
@@ -0,0 +1,31 @@
+var target = document.getElementById('clockDisplay');
+
+function showTime()
+{
+	var date = new Date();
+	var h = date.getHours();
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+	var session = "AM";
+
+	if(h == 0)
+	{
+		h = 12;
+	}
+	if (h > 12) { h = h-12;
+		session = "PM";
+	}
+
+h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+	var time = h +":" + m +":" + s +" " + session;
+	target.innerText = time;
+	target.textContent = time;
+	setTimeout(showTime, 1000);//The setTimeout() method calls a function or evaluates an expression after a specified number of milliseconds.
+
+}
+
+showTime();
+
