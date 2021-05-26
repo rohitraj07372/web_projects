c3aa9067ee35fb17a53925b32770d22a606e9be7
Rohit raj <rr2542333@gmail.com>
2021-05-26 22:43:21 +0530

    adding new features

--- magic game/script.js
+++ magic game/script.js
@@ -1,7 +1,7 @@
 var replay = document.getElementsByClassName('button2');
 var ok = document.getElementsByClassName('button1');
-var rand = Math.floor(Math.random()*11)+1;
-
+var rand = Math.floor(Math.random()*11)+2;
+var count = 0;
  // **************************  getting name  *********************************
 
 var a = prompt('Enter your name');
@@ -12,7 +12,7 @@ var a = prompt('Enter your name');
 
  // **************************** end og getting name **************************
 var i = 0;
-var txt = "Hello '"+a+"' My name is Rohit, let's play a magical game, PRESS OK";
+var txt = "Hello "+a+" My name is "  + "Rohit, let's play a magical game, Press ok";
 var speed = 50;
 
 var msg = document.getElementById('message');
@@ -27,20 +27,23 @@ function typeWriter() {
 
  function reset()
  {
-     msg.innerHTML=" ";
+      
  }
 
-var msg1 =["Think any amount in RUPEE bteween 1 - 10.", "Take the same amount from your friend and add them",
-"Now add rupee" + rand + "to your number","suppose you spend half of the money in bar",
+var msg1 =["Think any amount in Rupee bteween 1 - 10.", "Take the same amount from your friend and add them",
+"Now add rupee " + rand + " to your number","suppose you spend half of the money in bar",
 "Now return the money that you have taken form your friend form the remaining money",
-"YOU HAVE "+ rand/2 + "rupee with you! isn't it cool"] ;
+"You have "+"<b>"+ rand/2+"</b>"  + " rupee with you! isn't it cool"] ;
  
 
  
 
   function next(){
-  	 
-
+        if(count<=msg1.length)
+        {
+          msg.innerHTML = msg1[count];
+          count++;
+        }
    }
 
 
