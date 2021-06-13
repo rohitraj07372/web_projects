0e9f24c43462d8157f872fc48957ee5784d3abab
Rohit raj <rr2542333@gmail.com>
2021-06-13 07:03:06 +0530

    covid19

---
+++ Covid 19/script.js
@@ -0,0 +1,15 @@
+// fetch("https://covid-19-tracking.p.rapidapi.com/v1", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "74a99a9ad9msh245f5ef813065c1p11999ejsn7d1a54d44ca8",
+// 		"x-rapidapi-host": "covid-19-tracking.p.rapidapi.com"
+// 	}
+// })
+// .then (function(response) {
+//     return response.json(); 
+// }).then(function(result){
+//   console.log(result);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
